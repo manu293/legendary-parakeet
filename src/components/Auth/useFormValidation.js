458972b701ebd39926ee235ce34a299771a487d1
Manoj S M <manojsm293@gmail.com>
2020-01-13 00:11:29 +0530

    Created custom hooks
    Created custom login, signup and authentication

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,48 @@
-import React from 'react';
+// imports
+import React, { useState, useEffect } from 'react';
 
-function useFormValidation() {}
+// local imports
 
-export { useFormValidation };
+function useFormValidation(initialState, validate) {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSutbmitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (isSutbmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        console.log('User Authenticated');
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  function handleChange(event) {
+    event.persist();
+    setValues(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  function handleBlur() {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+    setValues({ ...initialState });
+    console.log({ values });
+  }
+
+  return { handleChange, handleSubmit, values, handleBlur, errors, isSutbmitting };
+}
+
+export default useFormValidation;
